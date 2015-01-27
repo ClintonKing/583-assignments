f1d6bc5742226397d867a2a435c556ed13206e7b
ClintonKing <kingcj@live.unc.edu>
2015-01-27 12:06:06 -0500

    Assignment4 added.

---
+++ assignment4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+my_name = 'Clinton King'
+my_school = 'UNC-Chapel Hill'
+classes = 5
+my_work = 'Daily Tar Heel'
+my_birthday = 28
+my_month = 'January'
+hours_per_class = 3
+total_hours = 3 * 5
+
+print classes * hours_per_class
+print "Yes, I have %d hours this semester." % total_hours
+print"Twenty-eight divided by four plus two minus five equals..."
+print my_birthday / 4 + 2 - classes
+
+print "My name is %s and I'm a student at %s." % (my_name, my_school)
+print "My birthday is %s %d." % (my_month, my_birthday)
+
+print 5**8
+
